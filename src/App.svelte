771926ae2c0a22f3e4e28e1d14d3cf0d9ec73c3e
Davner <davner23@gmail.com>
2022-05-15 17:39:39 -0400

    Atualização do arquivo svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
 
 <main>
 	<h1>Hello {name}!</h1>
+	<h5>Developed by Davner F. Cavalcante</h5>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
