9231df6105791f263c3d3c562b0424b7223f47ae
Davner <davner23@gmail.com>
2022-05-16 02:05:09 -0400

    Atualização 16/05/2022 - Anexo dos arquivos das fontes e do global.css

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,6 @@
 	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<h5>Developed by Davner F. Cavalcante</h5>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
 	main {
@@ -15,18 +10,22 @@
 		max-width: 240px;
 		margin: 0 auto;
 	}
-
+	
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
-
+	
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
 	}
-</style>
+	</style>
 
+<main>
+	<h1>Hello {name}!</h1>
+	<i class="fas fa-home fa-5x"></i>
+</main>
