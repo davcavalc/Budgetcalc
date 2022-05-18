1b2944dd768e1244d48aba810f53fc9968c88238
Davner <davner23@gmail.com>
2022-05-18 14:55:59 -0400

    Inserção de novos 'props' no App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script>
 import Navbar from "./navbar.svelte";
+import Title from "./title.svelte";
 </script>
 
 <Navbar />
+<Title title="add expense" />
+<Title title="expense list" />
 
 <style>
 </style>
