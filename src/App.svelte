ca9866ef669cd22f69d94892b2492e477b3b0275
Davner <davner23@gmail.com>
2022-05-18 15:07:04 -0400

    Atualização 2

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@ import Navbar from "./navbar.svelte";
 import Title from "./title.svelte";
 </script>
 
-<Navbar />
+<Navbar titleApp="Budget Calculator" buttonAddItem="add item"/>
 <Title title="add expense" />
 <Title title="expense list" />
 
--- src/navbar.svelte
+++ src/navbar.svelte
@@ -1,8 +1,13 @@
+<script>
+  export let titleApp;
+  export let buttonAddItem;
+</script>
+
 <nav class="nav">
     <div class="nav-center">
-      <h1 class="nav-title">Budget Calculator</h1>
+      <h1 class="nav-title">{titleApp}</h1>
       <button type="button" class="nav-btn">
-        <i class="far fa-plus-square">Add item</i>
+        <i class="far fa-plus-square">{buttonAddItem}</i>
       </button>
     </div>
   </nav>
